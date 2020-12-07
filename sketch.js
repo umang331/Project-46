f825063e3a4866b24ab768d8166f82996d8fc8b8
Umang balicha <54236975+umang331@users.noreply.github.com>
2020-12-07 14:16:57 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,16 +3,16 @@ var back,player,obstacle1,obstacle2,obstacle3,obstacle4,obstacle5,obstacle6,obst
 var obs4,obs4_1,obs4_2,obs4_3,edge1,edge2,edge3,edge4;
 
 function preload(){
-  pc = loadImage("../images/gunman2.png");
-  npc = loadImage("../images/gunman1.png");
-  deadManimg = loadImage("../images/npc.png");
-  backGround = loadImage("../images/background.png");
-  obs1 = loadImage("../images/obstacle1.png");
-  obs3 = loadImage("../images/obstacle3.png");
-  obs4 = loadImage("../images/obstacle4.png");
-  obs4_1 = loadImage("../images/obstacle41.png");
-  obs4_2 = loadImage("../images/obstacle42.png");
-  obs4_3 = loadImage("../images/obstacle43.png");
+  pc = loadImage("images/gunman2.png");
+  npc = loadImage("images/gunman1.png");
+  deadManimg = loadImage("images/npc.png");
+  backGround = loadImage("images/background.png");
+  obs1 = loadImage("images/obstacle1.png");
+  obs3 = loadImage("images/obstacle3.png");
+  obs4 = loadImage("images/obstacle4.png");
+  obs4_1 = loadImage("images/obstacle41.png");
+  obs4_2 = loadImage("images/obstacle42.png");
+  obs4_3 = loadImage("images/obstacle43.png");
   
 }
 
